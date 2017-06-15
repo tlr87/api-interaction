6715dd7e2cf23254fef948823b32c8a7ce2f74bf
Tom Revill <tom.revill@gmail.com>
2017-06-16 10:42:12 +1200

    Fixes

--- utilities.js
+++ utilities.js
@@ -1,3 +1,13 @@
 function reloadPage() {
     location.reload();
 }
+
+$(document).ready(function(){
+    var classCycle=['image1','image2', 'image3', 'image4'];
+
+    var backImage = Math.floor(Math.random() * classCycle.length);
+    var currentImage = classCycle[backImage];
+
+    $('body').addClass(currentImage);
+
+});
